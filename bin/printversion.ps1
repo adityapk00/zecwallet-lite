6498498ea1090589860177322d13c00008176d46
Aditya Kulkarni <adityapk@gmail.com>
2020-11-18 13:20:26 -0800

    Use new github env formats

--- bin/printversion.ps1
+++ bin/printversion.ps1
@@ -1,1 +1,1 @@
-echo "::set-env name=VERSION::1.3.3"
+echo "VERSION=1.3.3" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
--- bin/printversion.sh
+++ bin/printversion.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 VERSION="1.3.3"
-
-echo "::set-env name=VERSION::$VERSION"
+echo "VERSION=$VERSION" >> $GITHUB_ENV
