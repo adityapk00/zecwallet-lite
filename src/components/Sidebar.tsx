afee7375caf873ebaba624609196ddd025a4f04a
Aditya Kulkarni <adityapk@gmail.com>
2021-10-14 10:51:19 -0700

    v1.7.8

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -276,7 +276,7 @@ class Sidebar extends PureComponent<Props & RouteComponentProps, State> {
       openErrorModal(
         "Zecwallet Lite",
         <div className={cstyles.verticalflex}>
-          <div className={cstyles.margintoplarge}>Zecwallet Lite v1.7.5</div>
+          <div className={cstyles.margintoplarge}>Zecwallet Lite v1.7.8</div>
           <div className={cstyles.margintoplarge}>Built with Electron. Copyright (c) 2018-2021, Aditya Kulkarni.</div>
           <div className={cstyles.margintoplarge}>
             The MIT License (MIT) Copyright (c) 2018-2021 Zecwallet
